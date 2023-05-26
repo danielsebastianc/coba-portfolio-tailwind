42c9d00243426d04a5a55297cb66bbf3eb02df5b
danielsebastianc <danielsebastian207@gmail.com>
2023-05-26 12:42:27 +0700

    update dark mode and fixing layout

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,16 +5,55 @@ const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+  navMenu.classList.toggle("drop-down-active");
 });
 
 // navbar fixed
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.scrollY > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
   }
 };
+
+// close on click dropdown
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+    navMenu.classList.remove("drop-down-active");
+  }
+});
+
+// dark mode toggle
+const toggleBox = document.querySelector("#dark-mode-toggle");
+const html = document.querySelector("html");
+
+toggleBox.addEventListener("click", function () {
+  if (toggleBox.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// move toggle on refresh
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  toggleBox.checked = true;
+} else {
+  toggleBox.checked = false;
+}
